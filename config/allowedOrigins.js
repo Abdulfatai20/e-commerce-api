f641fae17fa71f2664e3ab13c4476dba1f25aba4
unknown <abdulfataimahmood35@gmail.com>
2025-10-05 19:51:41 +0100

    Changes

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "https://forever-frontend-one-gamma.vercel.app",
   "https://forever-admin-one-chi.vercel.app",
+  "http://localhost:5173",
 ];
 
-export default allowedOrigins
+export default allowedOrigins;
